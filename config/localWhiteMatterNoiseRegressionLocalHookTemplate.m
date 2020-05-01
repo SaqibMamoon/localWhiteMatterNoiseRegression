9fe5b3598725a1de49d77dfe96dadfa4a386a8e7
ozenctaskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2020-05-01 11:39:13 -0400

    Some instructions on how to install FSL and Freesurfer

--- config/localWhiteMatterNoiseRegressionLocalHookTemplate.m
+++ config/localWhiteMatterNoiseRegressionLocalHookTemplate.m
@@ -13,11 +13,37 @@ function localWhiteMatterNoiseRegressionLocalHook
 % You will need to edit the project location and i/o directory locations
 % to match what is true on your computer.
 
+%% Requires FSL and Freesurfer installation
+% For MAC
+
+% Make sure you have Quartz 2.7.6. If not, download it from here
+% http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg
+
+% Download Freesurfer installer from here and install by running it 
+% https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.0.0/freesurfer-darwin-macOS-7.0.0.pkg
+
+% Set Path to freesurfer. Copy these to .bashrc if you don't want to do this everytime
+% you open a terminal
+% export FREESURFER_HOME=/Applications/freesurfer
+% source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+% Copy license into /Application/freesurfer folder. License can be found in demo folder of localWhite repo
+% called license.txt
+
+% Make usre the installation worked by calling:
+% mri_convert from the terminal 
+
+% Install FSL
+% Installer is in the demo folder of localWhite repo. cd into that and run:
+% python2 fslinstaller.py
+% Follow the instructions 
+
+% Make sure you can run it by calling a function from the terminal:
+% flirt -version
  
 %% Define project
 projectName = 'localWhiteMatterNoiseRegression';
  
-
 %% Clear out old preferences
 if (ispref(projectName))
     rmpref(projectName);
